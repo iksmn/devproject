185d50b279fbd8b61b2433612568bc52f04f9afd
Icaro <iksmn@proton.me>
2025-04-01 14:05:17 -0300

    Reset

--- C/cap01/welcome.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-int main (void)
-{
-    printf("Bem-vindo a C!\n");
-    
-    return (0);
-}
--- Java/cap01/welcome.java
+++
@@ -1,7 +0,0 @@
-
-public class welcome {
-    public static void main (String [] args){
-        //Display message!
-        System.out.println ("Welcome to Java on Android");
-    }
-}
